f9e3fb2fdeb13381b942f6cd59a50e0e3e5fe459
Matteo Curia <107918712+MatteoCu@users.noreply.github.com>
2022-06-29 23:23:52 +0200

    fixato il foreach

--- lib/models/result.dart
+++ lib/models/result.dart
@@ -1,49 +1,72 @@
+
+
 class ReserchResult {
-  late String time;
-  late List<Elements> elements;
-
-  ReserchResult({required this.time,required this.elements});
-
-  ReserchResult.fromJson(Map<String, dynamic> json) {
+  ReserchResult({
+    required this.time,
+    required this.elements,
+  });
+  late final String time;
+  late final List<Elements> elements;
+  
+  ReserchResult.fromJson(Map<String, dynamic> json){
     time = json['time'];
-    if (json['elements'] != null) {
-      json['elements'].forEach((v) {
-        elements.add(Elements.fromJson(v));
-      });
-    }
+    elements = List.from(json['elements']).map((e)=>Elements.fromJson(e)).toList();
+  }
+
+  Map<String, dynamic> toJson() {
+    final _data = <String, dynamic>{};
+    _data['time'] = time;
+    _data['elements'] = elements.map((e)=>e.toJson()).toList();
+    return _data;
   }
 }
 
 class Elements {
-  late String descriptiom;
-  late List<String> price;
-  late int value;
-  late List<Comments> comments;
-
-  Elements({required this.descriptiom,required this.price,required this.value,required this.comments});
-
-  Elements.fromJson(Map<String, dynamic> json) {
-    descriptiom = json['descriptiom'];
-    price = json['price'].cast<String>();
+  Elements({
+    required this.description,
+    required this.value,
+    required this.price,
+    required this.comments,
+  });
+  late final String description;
+  late final int value;
+  late final List<String> price;
+  late final List<Comments> comments;
+  
+  Elements.fromJson(Map<String, dynamic> json){
+    description = json['description'];
     value = json['value'];
-    if (json['comments'] != null) {
-      json['comments'].forEach((v) {
-        comments.add(Comments.fromJson(v));
-      });
-    }
+    price = List.castFrom<dynamic, String>(json['price']);
+    comments = List.from(json['comments']).map((e)=>Comments.fromJson(e)).toList();
   }
 
+  Map<String, dynamic> toJson() {
+    final _data = <String, dynamic>{};
+    _data['description'] = description;
+    _data['value'] = value;
+    _data['price'] = price;
+    _data['comments'] = comments.map((e)=>e.toJson()).toList();
+    return _data;
+  }
 }
 
 class Comments {
-  late String name;
-  late String text;
-
-  Comments({required this.name,required this.text});
-
-  Comments.fromJson(Map<String, dynamic> json) {
+  Comments({
+    required this.name,
+    required this.text,
+  });
+  late final String name;
+  late final String text;
+  
+  Comments.fromJson(Map<String, dynamic> json){
     name = json['name'];
     text = json['text'];
   }
 
+  Map<String, dynamic> toJson() {
+    final _data = <String, dynamic>{};
+    _data['name'] = name;
+    _data['text'] = text;
+    return _data;
+  }
 }
